f7a703341a4ee2d20250b52861302f28567931ee
jyotsana123 <jyotsana.branddirect@gmail.com>
2023-04-16 19:44:13 +0530

    ForLoop

---
+++ src/s4.java
@@ -0,0 +1,129 @@
+
+public class s4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		/*   * * * *
+		 *    * * *
+		 *     * *
+		 *      *
+		 */
+		
+		for(int i=1;i<=4;i++)
+		{
+			for (int j=1;j<=i;j++)
+			{
+				System.out.print(" ");
+			}
+			for (int k=i;k<=4;k++)
+			{
+				System.out.print("* ");	
+			}
+			System.out.println("");
+		}
+		
+		
+		/*       *
+		 *      * *
+		 *     * * *
+		 *    * * * *
+		 */
+		
+		for(int i=1;i<=4;i++)
+		{
+			for (int j=i;j<=4;j++)
+			{
+				System.out.print(" ");
+			}
+			for (int k=1;k<=i;k++)
+			{
+				System.out.print("* ");	
+			}
+			System.out.println("");
+		}
+		
+		
+		/*         *
+		 *       * *
+		 *     * * *
+		 *   * * * *
+		 */
+		
+		for(int i=1;i<=4;i++)
+		{
+			for (int j=i;j<=4;j++)
+			{
+				System.out.print("  ");
+			}
+			for (int k=1;k<=i;k++)
+			{
+				System.out.print("* ");	
+			}
+			System.out.println("");
+		}
+		
+		
+		/*   * * * *
+		 *   * * * 
+		 *   * *
+		 *   * 
+		 */
+		
+		for(int i=1;i<=4;i++)
+		{
+			for (int j=i;j<=4;j++)
+			{
+				System.out.print("* ");
+			}
+			System.out.println("");
+		}
+		
+		
+		/*        *
+		 *      * * *
+		 *    * * * * *
+		 *  * * * * * * *
+		 */
+		
+		for(int i=1;i<=4;i++)
+		{
+			for (int j=i;j<=4;j++)
+			{
+				System.out.print("  ");
+			}
+			for(int k=1;k<=i;k++)
+			{
+			System.out.print("* ");
+			}
+			for(int r=2;r<=i;r++)
+			{
+				System.out.print("* ");
+			}
+			System.out.println("");
+		}
+		
+		
+		/*   *
+		 *   * * * 
+		 *   * * * * *
+		 *   * * * * * * *
+		 */
+		
+		for(int i=1;i<=4;i++)
+		{
+			for (int j=1;j<=i;j++)
+			{
+				System.out.print("* ");
+			}
+			for (int k=2;k<=i;k++)
+			{
+				System.out.print("* ");
+			}
+			
+			System.out.println("");
+		}
+		
+	}
+
+}
